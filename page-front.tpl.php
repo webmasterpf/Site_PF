01f09c75e57afa039f9ca8562fbeec43214098f0
webmasterpf-dg <webmaster@provence-formation.com>
2009-12-07 14:20:12 +0100

    Envoi des fichiers sources du theme drupal pour le site PF
    Signed-off-by: webmasterpf-dg <webmaster@provence-formation.com>

---
+++ page-front.tpl.php
@@ -0,0 +1,78 @@
+<?php include "header.tpl.php"; ?>
+ <!-- ______________________ CONTENU _______________________ -->
+    
+	<div id="mainPage">
+	
+      <div id="contentHome">
+   
+
+          <?php if ($content_top): ?>
+            <div id="content-top">
+              <?php print $content_top; ?>
+            </div> <!-- /#content-top -->
+          <?php endif; ?>
+
+          <?php if ($breadcrumb || $title || $mission || $messages || $help || $tabs): ?>
+            <div id="content-header">
+
+              
+
+              <?php if ($title): ?>
+                <h1 class="title"><?php print $title; ?></h1>
+              <?php endif; ?>
+
+              <?php if ($mission): ?>
+                <div id="mission"><?php print $mission; ?></div>
+              <?php endif; ?>
+
+              <?php print $messages; ?>
+
+              <?php print $help; ?> 
+
+              <?php if ($tabs): ?>
+                <div class="tabs"><?php print $tabs; ?></div>
+              <?php endif; ?>
+
+            </div> <!-- /#content-header -->
+          <?php endif; ?>
+
+          <div id="content-area">
+            <?php print $content; ?>
+          </div> <!-- /#content-area -->
+
+          <?php print $feed_icons; ?>
+
+         
+
+          </div><!-- /content-inner /content -->
+		  
+		  
+	  
+	  
+	   <!-- ______________________ EDITO DROITE _______________________ -->
+      
+	  
+	  <div id="contentEdito">
+       
+		
+		  <?php if ($edito): ?>
+        <div id="textEdito">
+		
+          <?php print $edito; ?>
+        </div><!-- /#textEdito -->
+        <?php endif; ?>
+		
+      </div><!-- /#contentEdito -->
+	  
+   
+	<br clear="all">
+	
+    <?php if ($content_bottom): ?>
+    <div id="content-bottom">
+      <?php print $content_bottom; ?>
+    </div><!-- /#content-bottom -->
+    <?php endif; ?>
+	
+	
+	 </div> <!-- /mainPage -->
+      <?php include "footer.tpl.php";?>
